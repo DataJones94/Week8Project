c5316eaefb280d45b89792fc2c4c4e429f4c8993
unknown <k.jones45@yahoo.com>
2023-02-17 11:21:06 -0500

    Created Functionality to Update a sepecific product in the Products App view file

--- products/views.py
+++ products/views.py
@@ -1,8 +1,11 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
+
+
 
 @api_view(['GET', 'POST'])
 def products_list(request):
@@ -11,28 +14,27 @@ def products_list(request):
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
 
-
-
     elif request.method == 'POST':    
-        serializer = ProductSerializer(data=request.data)
-        
-        if serializer.is_valid() == True:
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
+        serializer = ProductSerializer(data=request.data)        
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+           
 
-@api_view (['GET'])
+@api_view (['GET', 'PUT'])
 def product_detail(request, pk):
-    try:
-        product = Product.objects.get(pk=pk)
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'GET':
         serializer = ProductSerializer(product);
         return Response(serializer.data)   
-         
-    except Product.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND);    
+    elif request.method == 'PUT':    
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
-    print(pk)
-    return Response(pk)
+
+
+    
 
 
